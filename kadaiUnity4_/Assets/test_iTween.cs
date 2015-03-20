ff34e9971376f2ce795abaa0e38ac9ad29c6a253
akikan-alumi <y_r9fj22sdf@live.jp>
2015-03-20 22:15:43 +0900

    unityでTRPGを再現できるようにするプロジェクトの第一歩

---
+++ kadaiUnity4_/Assets/test_iTween.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class test_iTween : MonoBehaviour {
+    public GameObject gameobject;
+    Hashtable table = new Hashtable();      // ハッシュテーブルを用意
+
+    private void Start() {
+        table.Add("x", 10);            // xを10まで移動
+        table.Add("y", 5);         // yを5まで移動
+        table.Add("time", 1.0f);        // トゥイーン時間は3秒
+        table.Add("delay", 1.0f);       // 1秒遅らせてからトゥイーンスタート
+        table.Add("oncomplete", "CompleteHandler");
+        table.Add("oncompletetarget", gameObject);
+
+        iTween.MoveTo(gameObject, table);   // 第二引数にハッシュテーブルをセット
+        //iTween.MoveTo(gameObject, iTween.Hash("x", 20, "looptype", iTween.LoopType.loop));
+        // 4秒かけて、y軸を260度回転
+        //iTween.RotateTo(gameObject, iTween.Hash("y", 260, "time", 4.0f));
+        // 4秒かけて、y軸を3倍に拡大
+        //iTween.ScaleTo(gameObject, iTween.Hash("y", 3, "time", 4.0f));
+    }
+
+    private void OnTriggerStay(Collider other) {
+        Debug.Log("OnTriggerStay");
+    }
+
+    private void StartHandler() {
+        Debug.Log("StartHandler");
+    }
+
+    private void UpdateHandler() {
+        Debug.Log("UpdateHandler");
+    }
+
+    private void CompleteHandler() {
+        Debug.Log("CompleteHandler");
+    }
+
+}
---
+++ kadaiUnity4_/Assets/trace.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class trace : MonoBehaviour {
+    // 開始位置のオブジェクト
+    //public GameObject StartPosition;
+    private RaycastHit hit;
+    private Vector3 normalVec3 = new Vector3(0.5f,0.5f,0.5f);
+    // Use this for initialization
+    void Start() {
+        // 開始位置をとりあえずゴールを開始位置のオブジェクトの位置に
+
+        hit.point = normalVec3;
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetMouseButtonDown(0)) {
+            // クリックした画面から半直線(ray)を生成
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            // 半直線にヒットした情報をhit変数に格納
+            Physics.Raycast(ray, out hit);
+        }
+        // 位置を更新し続ける
+        iTween.MoveUpdate(gameObject, hit.point, 1.0f);
+    }
+}
