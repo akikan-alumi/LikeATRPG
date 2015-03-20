3f54f7a513debb3eaa68b857a2f96b321f29bf03
akikan-alumi <y_r9fj22sdf@live.jp>
2015-03-21 01:19:54 +0900

    とりあえずClickで動く程度まで

--- kadaiUnity4_/Assets/trace.cs
+++ kadaiUnity4_/Assets/trace.cs
@@ -7,12 +7,15 @@ public class trace : MonoBehaviour {
     //public GameObject StartPosition;
     private RaycastHit hit;
     private Vector3 normalVec3 = new Vector3(0.5f,0.5f,0.5f);
+    [SerializeField]
+    private Vector3 hitVec3;
     // Use this for initialization
     void Start() {
         // 開始位置をとりあえずゴールを開始位置のオブジェクトの位置に
-
+        
         hit.point = normalVec3;
-
+        this.transform.position = normalVec3;
+        hitVec3 = normalVec3;
     }
 
     // Update is called once per frame
@@ -22,8 +25,9 @@ public class trace : MonoBehaviour {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             // 半直線にヒットした情報をhit変数に格納
             Physics.Raycast(ray, out hit);
+            hitVec3 = new Vector3(hit.point.x, hit.point.y + 0.5f, hit.point.z);
         }
         // 位置を更新し続ける
-        iTween.MoveUpdate(gameObject, hit.point, 1.0f);
+        iTween.MoveUpdate(gameObject, hitVec3, 1.0f);
     }
 }
