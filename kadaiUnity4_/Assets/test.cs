674fe3b97430f8c6310e8f5dc5c6760ec8637d3f
akikan-alumi <y_r9fj22sdf@live.jp>
2015-03-29 01:28:49 +0900

    unityから外部ウィンドウを開いてファイルを開くことに成功

--- kadaiUnity4_/Assets/test.cs
+++ kadaiUnity4_/Assets/test.cs
@@ -1,17 +1,37 @@
 ﻿using UnityEngine;
 using System.Collections;
-
-///dllのテスト
-using testdll;
+using UnityEditor;
 /// <summary>
-/// 
+/// unityの公式にあるUnityScriptでのOpenFilePanelのクラスをがんばってC#にした。
+/// 最初はstartにどちらも入れていたのだが、どうやら20行目あたりにあるreturn文が悪さをするので
+/// AwakeとStartの2つに分けた。
+/// 参考URL：http://docs.unity3d.com/ja/current/ScriptReference/EditorUtility.OpenFilePanel.html
 /// </summary>
 public class test : MonoBehaviour {
-
+    private EditorUtility eu;
+    public Texture2D tx;
+    void Awake() {
+        eu = new EditorUtility();
+        tx = Selection.activeObject as Texture2D;
+        if (tx == null) {
+            EditorUtility.DisplayDialog(
+                "SelectTexture",
+                "You Must Select a Texture first!",
+                "OK");
+            return;
+        }
+    }
 	// Use this for initialization
 	void Start () {
-        Debug.Log(testdll.MyClass.message());
         
+        var path = EditorUtility.OpenFilePanel(
+            "Overwrite with pnf",
+            "",
+            "png");
+        if (path.Length != 0) {
+            var www = new WWW("file:///" + path);
+            www.LoadImageIntoTexture(tx);
+        }
 	}
 	
 	// Update is called once per frame
