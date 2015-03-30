ec9d4e152a0ddd3787cba6884792340cde0d5944
akikan-alumi <y_r9fj22sdf@live.jp>
2015-03-31 00:07:39 +0900

    取り込んだ画像を2Dスプライト用に変換するコードに変更。ボタンを追加し、それを押すことで画像を選べるように変更。

--- kadaiUnity4_/Assets/test.cs
+++ kadaiUnity4_/Assets/test.cs
@@ -5,32 +5,37 @@ using UnityEditor;
 /// unityの公式にあるUnityScriptでのOpenFilePanelのクラスをがんばってC#にした。
 /// 最初はstartにどちらも入れていたのだが、どうやら20行目あたりにあるreturn文が悪さをするので
 /// AwakeとStartの2つに分けた。
+/// 03/30追記
+/// Start廃止してchangeTextureに変更
 /// 参考URL：http://docs.unity3d.com/ja/current/ScriptReference/EditorUtility.OpenFilePanel.html
 /// </summary>
 public class test : MonoBehaviour {
     private EditorUtility eu;
     public Texture2D tx;
+    public GameObject texture;
     void Awake() {
         eu = new EditorUtility();
         tx = Selection.activeObject as Texture2D;
         if (tx == null) {
             EditorUtility.DisplayDialog(
                 "SelectTexture",
-                "You Must Select a Texture first!",
+                "テクスチャが設定されてません",
                 "OK");
             return;
         }
     }
 	// Use this for initialization
-	void Start () {
+	public void changeTexture () {
         
         var path = EditorUtility.OpenFilePanel(
-            "Overwrite with pnf",
+            "Overwrite with jpg",
             "",
-            "png");
+            "jpg");
         if (path.Length != 0) {
             var www = new WWW("file:///" + path);
-            www.LoadImageIntoTexture(tx);
+            tx = www.textureNonReadable;
+            Sprite spriteTexture = Sprite.Create(tx ,new Rect(0,0,255,255),Vector2.zero);
+            texture.GetComponent<SpriteRenderer>().sprite = spriteTexture;
         }
 	}
 	
