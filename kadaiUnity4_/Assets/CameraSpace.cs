06a7418c101449b77fc465b780a7305f76f8d1a1
akikan-alumi <y_r9fj22sdf@live.jp>
2015-04-03 23:34:23 +0900

    カメラの座標軸とりに四苦八苦、現在も奮闘中

--- kadaiUnity4_/Assets/CameraSpace.cs
+++ kadaiUnity4_/Assets/CameraSpace.cs
@@ -4,17 +4,27 @@ using System.Collections;
 public class CameraSpace : MonoBehaviour {
     public Camera UIcamera;
     public Camera mainCamera;
+
+    private Vector3 mainVec;
+    private Vector3 UIVec;//必要ないかもだけど一応
+
+    private Vector3 rightVec;//左右にカメラを動かすときに使う変数
 	// Use this for initialization
 	void Awake () {
         mainCamera.rect = new Rect(0,0,0.8f,1);
         UIcamera.rect = new Rect(0.8f, 0, 0.8f, 1);
+        mainVec = mainCamera.transform.position;
+        UIVec = UIcamera.transform.position;
+        rightVec = new Vector3(1f, 0f, 0f);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+        if (mainCamera.transform.position != mainVec) {
+
+        }
 	}
     public void leftMove() {
-       // mainCamera.transform.position += ;
+        mainCamera.transform.position -= rightVec;
     }
 }
--- kadaiUnity4_/Assets/trace.cs
+++ kadaiUnity4_/Assets/trace.cs
@@ -11,17 +11,20 @@ public class trace : MonoBehaviour {
 
     public Camera mainCamera;//かめらの位置取得用
 
-    private float negativeMargin = 0;
-    private float positiveMargin = 1f;
-    private float marginX = 1280;
-    private float marginY = 720;
+    private float negativeMargin = 0f;
+    //private float positiveMargin = 1f;
+    private float marginX = 1280f;
+    private float marginY = 720f;
+
+    private float mySpeed;//トゥウィーンで動くときのスピード
+
     // Use this for initialization
     void Start() {
+        mySpeed = 1f;
         // 開始位置を画面中心辺りにする        
         hit.point = normalVec3;
         this.transform.position = normalVec3;
         hitVec3 = normalVec3;
-        //testdll.MyClass.message();
     }
 
     // Update is called once per frame
@@ -29,30 +32,30 @@ public class trace : MonoBehaviour {
         if (Input.GetMouseButtonDown(0)) {
             // クリックした画面から半直線(ray)を生成
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            //Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
+            //Ray ray = Camera.main.ViewportPointToRay(Input.mousePosition);
             if (isOutOfScreen(ray.origin)) {
+            print("mousePosition"+ray.origin);
                 // 半直線にヒットした情報をhit変数に格納
                 Physics.Raycast(ray, out hit);
+
                 hitVec3 = new Vector3(hit.point.x, hit.point.y + 0.5f, hit.point.z);
                 print("真");
-                print(transform.position);
+                print("playerの現在位置"+transform.position);
             } else {
                 print("偽");
-                print(transform.position);
-                //hitVec3 = this.transform.position;
+                print("playerの現在位置" + transform.position);
             }
         }
         // 位置を更新し続ける
-        iTween.MoveUpdate(gameObject, hitVec3, 0.1f);
+        iTween.MoveUpdate(gameObject, hitVec3, mySpeed);
     }
 
     private bool isOutOfScreen(Vector3 vec3) {
-
-        if (vec3.x <= negativeMargin ||//フィールドが表示されている部分
-            vec3.x >= marginY ||
-            vec3.y <= negativeMargin ||
-            vec3.y >= marginX) {
+        print("カメラからみたクリック場所取得" + vec3);
+        if ((vec3.x >= negativeMargin ||//フィールドが表示されている部分
+            vec3.x <= marginX) &&
+            (vec3.y >= negativeMargin ||
+            vec3.y <= marginY)) {
             return true;
         }else{
             return false;
