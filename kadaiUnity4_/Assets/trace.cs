3098cf246edb2c1adcb41b2a98f179e45c753b00
akikan-alumi <y_r9fj22sdf@live.jp>
2015-04-04 01:22:15 +0900

    とりあえずは画面右の座標を取得しないようにできた。そろそろ別の実装を行わないといけない。

--- kadaiUnity4_/Assets/trace.cs
+++ kadaiUnity4_/Assets/trace.cs
@@ -13,10 +13,14 @@ public class trace : MonoBehaviour {
 
     private float negativeMargin = 0f;
     //private float positiveMargin = 1f;
-    private float marginX = 1280f;
-    private float marginY = 720f;
+    private float marginX = Screen.width * 0.8f;
+    private float marginY = Screen.height;
 
     private float mySpeed;//トゥウィーンで動くときのスピード
+
+    void Awake() {
+        print("スクリーンの大きさ　X：" + marginX + "   Y:"+marginY);
+    }
 
     // Use this for initialization
     void Start() {
@@ -31,19 +35,18 @@ public class trace : MonoBehaviour {
     void Update() {
         if (Input.GetMouseButtonDown(0)) {
             // クリックした画面から半直線(ray)を生成
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            //Ray ray = Camera.main.ViewportPointToRay(Input.mousePosition);
-            if (isOutOfScreen(ray.origin)) {
-            print("mousePosition"+ray.origin);
+            Vector3 vec3 = Input.mousePosition;
+            if (isOutOfScreen(vec3)) {
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
                 // 半直線にヒットした情報をhit変数に格納
                 Physics.Raycast(ray, out hit);
 
                 hitVec3 = new Vector3(hit.point.x, hit.point.y + 0.5f, hit.point.z);
                 print("真");
-                print("playerの現在位置"+transform.position);
+                print("playerの現在位置transform"+transform.position);
             } else {
                 print("偽");
-                print("playerの現在位置" + transform.position);
+                print("playerの現在位置transform" + transform.position);
             }
         }
         // 位置を更新し続ける
@@ -52,9 +55,9 @@ public class trace : MonoBehaviour {
 
     private bool isOutOfScreen(Vector3 vec3) {
         print("カメラからみたクリック場所取得" + vec3);
-        if ((vec3.x >= negativeMargin ||//フィールドが表示されている部分
+        if ((vec3.x >= negativeMargin &&//フィールドが表示されている部分
             vec3.x <= marginX) &&
-            (vec3.y >= negativeMargin ||
+            (vec3.y >= negativeMargin &&
             vec3.y <= marginY)) {
             return true;
         }else{
